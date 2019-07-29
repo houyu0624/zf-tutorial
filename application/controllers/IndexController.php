7269025e57994cb7f72b92e375ece1378dde09e5
Yu Hou <Yu@YU-MacBook.local>
2019-07-28 22:03:58 -0700

    create album add form

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -16,7 +16,25 @@ class IndexController extends Zend_Controller_Action
 
     public function addAction()
     {
-        // action body
+        $form = new Application_Form_Album();
+        $form->submit->setLabel('Add');
+        $this->view->form = $form;
+
+        if ($this->getRequest()->isPost()) {
+            $formData = $this->getRequest()->getPost();
+
+            if ($form->isValid($formData)) {
+                $artist = $form->getValue('artist');
+                $title = $form->getValue('title');
+
+                $albums = new Application_Model_DbTable_Albums();
+                $albums->addAlbum($artist, $title);
+
+                $this->_helper->redirector('index');
+            } else {
+                $form->populate($formData);
+            }
+        }
     }
 
     public function editAction()
