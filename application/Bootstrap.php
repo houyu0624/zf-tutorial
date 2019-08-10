ee7484ab4d7086dbf7e0ab8e9061315ade86e45c
Yu Hou <houyu0624@hotmail.com>
2019-08-10 16:13:34 -0700

    Modify head title

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,22 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    protected function _initView()
+    {
+        // Initialize view
+        $view = new Zend_View();
+        $view->doctype('XHTML1_STRICT');
+        $view->headTitle('Framework');
 
+        // Add it to the ViewRenderer
+        $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper(
+            'ViewRenderer'
+        );
+        $viewRenderer->setView($view);
+
+        // Return it, so that it can be stored by the bootstrap
+        return $view;
+    }
 
 }
 
