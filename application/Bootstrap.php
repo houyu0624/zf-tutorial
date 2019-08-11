e9c5fc8775f5597d6b545d95e3f43761998cfd13
Yu Hou <houyu0624@hotmail.com>
2019-08-10 17:37:27 -0700

    hide framework text

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -7,7 +7,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         // Initialize view
         $view = new Zend_View();
         $view->doctype('XHTML1_STRICT');
-        $view->headTitle('Framework');
+        $view->headTitle('亲爱的');
 
         // Add it to the ViewRenderer
         $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper(
