cead2d4ed5263e46921deefd07e83942130a978a
Yu Hou <houyu0624@hotmail.com>
2019-08-11 22:23:56 -0700

    create rest controller

---
+++ application/modules/albums/controllers/RestController.php
@@ -0,0 +1,96 @@
+<?php
+
+class RestController extends Zend_Rest_Controller
+{
+    private $_form;
+
+    public function init()
+    {
+        $bootstrap = $this->getInvokeArg('bootstrap');
+
+        $options = $bootstrap->getOption('resources');
+
+        $contextSwitch = $this->_helper->getHelper('contextSwitch');
+        $contextSwitch->addActionContext('index', array('xml','json'))->initContext();
+
+        //$this->_helper->viewRenderer->setNeverRender();
+        $this->view->success = "true";
+        $this->view->version = "1.0"; //hard-coded for now.
+    }
+
+    /**
+     * The index action handles index/list requests; it should respond with a
+     * list of the requested resources.
+     */
+    public function indexAction()
+    {
+        //if you want to have access to a particular paramater use the helper function as follows:
+        //print $this->_helper->getParam('abc');
+        //To test with this use:  http://myURL/format/xml/abc/1002
+
+    }
+
+    /**
+     * The list action is the default for the rest controller
+     * Forward to index
+     */
+    public function listAction()
+    {
+        $this->forward('index');
+    }
+
+    /**
+     * The get action handles GET requests and receives an 'id' parameter; it
+     * should respond with the server resource state of the resource identified
+     * by the 'id' value.
+     */
+    public function getAction()
+    {
+        $this->forward('index');
+    }
+
+    /**
+     * Show the new request
+     */
+    public function newAction() {
+        $this->forward('index');
+    }
+
+    /**
+     * The post action handles POST requests; it should accept and digest a
+     * POSTed resource representation and persist the resource state.
+     */
+    public function postAction() {
+        $this->forward('index');
+    }
+
+    /**
+     * Show the edit book form. Url format: /version/edit/2
+     */
+    public function editAction() {
+        $this->forward('index');
+    }
+
+    /**
+     * The put action handles PUT requests and receives an 'id' parameter; it
+     * should update the server resource state of the resource identified by
+     * the 'id' value.
+     */
+    public function putAction() {
+        $this->forward('index');
+    }
+
+    /**
+     * The delete action handles DELETE requests and receives an 'id'
+     * parameter; it should update the server resource state of the resource
+     * identified by the 'id' value.
+     */
+    public function deleteAction() {
+        $this->forward('index');
+    }
+
+    public function headAction() {
+        $this->forward('index');
+    }
+
+}
